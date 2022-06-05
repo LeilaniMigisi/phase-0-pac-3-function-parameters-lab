b1a2faa6d7e2a04aabeba5a3f02a52583ea80931
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-06-05 16:32:12 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+const name= value1 (leilani);
+const language= value2 (javascript);
+function introductionWithLanguage(leilani, javascript){
+    console.log(`Hi, my name is${Name} and  am learning to program in${Language}.`);
+  }
